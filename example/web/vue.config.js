4786e6829f97e340ec6db1c7c83c5807e38a569f
eas恶客 <940920587@qq.com>
2026-06-09 14:57:46 +0800

    feat: first commit

---
+++ example/web/vue.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  indexPath: "../templates/index.html",
+  outputDir: "../static",
+  publicPath: process.env.PUBLIC_PATH,
+  assetsDir: "",
+  devServer: {
+    disableHostCheck: true,
+    port: 8888,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5000',
+        // target: 'http://127.0.0.1:6010',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/api'
+        }
+      }
+    }
+  },
+  lintOnSave: false,
+  transpileDependencies: [
+    'vue-echarts',
+    'resize-detector'
+  ]
+}
+
+console.log(module.exports);
+console.log(process.env);
