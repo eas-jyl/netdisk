a82b77b2b9be4f5e97db942432241852b2fb4449
eas恶客 <940920587@qq.com>
2026-06-10 19:55:08 +0800

    feat: 上传go-netdisk文件夹

---
+++ go-netdisk/demo/cmd/server/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+
+	"go-netdisk/internal/config"
+)
+
+func main() {
+	cfg, err := config.Load("config/dev.yaml")
+	if err != nil {
+		log.Fatalf("load config failed: %v", err)
+	}
+
+	log.Printf("app=%s port=%d mysql=%s:%d/%s",
+		cfg.App.Name,
+		cfg.App.Port,
+		cfg.MySQL.Host,
+		cfg.MySQL.Port,
+		cfg.MySQL.DBName,
+	)
+}
